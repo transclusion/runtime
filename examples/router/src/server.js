b1ea33b8513ea9752d7fb0c07b2d75273c3acb2c
Marius Lundgård <studio@mariuslundgard.com>
2017-12-01 00:38:32 +0100

    docs(example): Use async/await

--- examples/router/src/server.js
+++ examples/router/src/server.js
@@ -5,32 +5,32 @@ import * as root from './root'
 export function create({manifest}) {
   const app = express()
 
-  app.get('/*', (req, res) => {
-    run({
-      program: root,
-      props: {path: req.path}
-    })
-      .then(context => {
-        res.send(`<!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="/${manifest['browser.css']}">
-    </head>
-    <body>
-      <div id="root">${context.html}</div>
-      <script>
-      var __INITIAL_PROPS__ = ${JSON.stringify(context.model)}
-      </script>
-      <script src="/${manifest['browser.js']}"></script>
-    </body>
-    </html>`)
+  app.get('/*', async (req, res) => {
+    try {
+      const {html, model} = await run({
+        program: root,
+        props: {path: req.path}
       })
-      .catch(err => {
-        res.status(500)
-        res.send(err.stack)
-      })
+
+      res.send(`<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/${manifest['browser.css']}">
+</head>
+<body>
+  <div id="root">${html}</div>
+  <script>
+  var __INITIAL_PROPS__ = ${JSON.stringify(model)};
+  </script>
+  <script src="/${manifest['browser.js']}"></script>
+</body>
+</html>`)
+    } catch (err) {
+      res.status(500)
+      res.send(err.stack)
+    }
   })
 
   return app
